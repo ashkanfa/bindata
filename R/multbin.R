1f281ffef875d775dc7de58d88426836277ac3c7
Ashkan Farahani <38392052+ashkanfa@users.noreply.github.com>
2018-12-19 03:35:22 -0600

    Update multbin.R

--- R/multbin.R
+++ R/multbin.R
@@ -145,13 +145,13 @@ check.commonprob <- function (commonprob)
         ll <- max(commonprob[i,i]+commonprob[j,j]-1, 0)
         if (commonprob[i,j] > ul) 
           {
-            commoncommonprob[i,j] <- ul
+            commonprob[i,j] <- ul
             message[nm<-nm+1] <- 
               paste("Element (",i,",",j,"): changed to nearest Admissible upper limit [",ul,"].")
           }
         if (commonprob[i,j] < ll)
           {
-          commoncommonprob[i,j] <- ll
+          commonprob[i,j] <- ll
             message[nm<-nm+1] <- 
               paste("Element (",i,",",j,"): changed to nearest Admissible lower limit [",ll,"].")
           }
